6353bcd54a66ab16f8fa10a66934b57f9ab62141
Allex <inta.dn@yandex.ru>
2023-06-09 13:53:45 +0300

    Реализация createRequest

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,31 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+  const xhr = new XMLHttpRequest();
+  
+  xhr.responseType = 'json';
+  xhr.open(options.method, options.url);
 
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      options.callback(null, xhr.response);
+    } else {
+      options.callback(new Error(xhr.statusText), null);
+    }
+  };
+
+  xhr.onerror = () => {
+    options.callback(new Error('Network Error'), null);
+  };
+
+  if (options.method === 'GET') {
+    xhr.send();
+  } else {
+    const formData = new FormData();
+    for (let key in options.data) {
+      formData.append(key, options.data[key]);
+    }
+    xhr.send(formData);
+  }
 };
+
